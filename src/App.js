4322c577357802eef393d69484d3160df29bafc3
unknown <mohamed.benighil07@gmail.com>
2020-12-31 22:43:02 +0100

    first navbar component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+//import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import Navbar from './components/navbar/Navbar'
+
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <Navbar />
+      </div>
+    );
+  }
+
+}
+
+export default App;
