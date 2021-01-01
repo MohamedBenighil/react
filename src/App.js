6484b19397005f7fad33fc5318e3c58feb496b80
unknown <mohamed.benighil07@gmail.com>
2021-01-02 00:28:36 +0100

    fetch data in App and pass it to User

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 //import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
+import axios from 'axios'
 import Navbar from './components/navbar/Navbar'
 import Users from './components/users/Users'
 
+
 class App extends Component{
+  state = {
+    users: [],
+    loading: false
+  }
+  async componentDidMount(){
+    this.setState({loading: true})
+    const res = await axios.get("https://api.github.com/users")
+    this.setState({users: res.data,loding: false})
+  }
+
   render(){
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-        <Users />
+        <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
