87fb2cbe37d86718960577a3a61ec81f1dea471d
unknown <mohamed.benighil07@gmail.com>
2021-01-06 23:42:21 +0100

    Desing User component

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Fragment, Component } from 'react'
+import Spinner from '../spinner/Spinner'
+import {Link} from 'react-router-dom'
 
 class User extends Component {
     componentDidMount(){
@@ -7,24 +9,66 @@ class User extends Component {
     render() {
         const { 
             name,
+            hireable,
             avatar_url,
             location,
             bio,
             blog,
             login,
             html_url,
+            company,
+            website,
             followers,
             following,
             public_repos,
             public_gists
         } = this.props.user
+
         const {loading} = this.props.loading
 
-
+        if (loading){
+            return  <Spinner />
+        } 
+        
         return (
-            <div>
-                the name is : {name}
-            </div>
+            <Fragment>
+                <Link to={'/'} className={'btn btn-light'} > Back To Search </Link>
+                Hireable: {''}
+                {hireable ? <i className={'fas fa-check text-success'} /> : <i className={'fas fa-times-circle text-danger'} /> }
+                <div className="card grid-2">
+                    <div className="all-center">
+                        <img src={avatar_url} className="round-img" style={{width:'150px'}} alt=""/>
+                        <h1>{name}</h1>
+                        <p>Location : {location}</p>
+                    </div>
+                    <div>
+                        {bio && 
+                        <Fragment>
+                            <h3>Bio</h3>
+                            <p>{bio}</p>
+                        </Fragment>}
+                        <a href={html_url} className='btn btn-dark my-1'>Visit Github Profile</a>
+                        <ul>
+                            <li>{login && 
+                                <Fragment><b>Username</b>  : {login}</Fragment>}
+                            </li>
+                            <li>{company && 
+                                <Fragment><strong>Company</strong>: {company}</Fragment>}
+                            </li>
+                            <li>{website && 
+                                <Fragment> <b>WebSite</b>: {website}</Fragment>}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="card text-center">
+                    <div className="badge badge-primary">Followers: {followers}</div>
+                    <div className="badge badge-success">Following: {following}</div>
+                    <div className="badge badge-light">Public Repos: {public_repos}</div>
+                    <div className="badge badge-dark">Public Gests: {public_gists}</div>
+                </div>
+            </Fragment>
+
         )
     }
 }
