cb1fb8d290bb6767030b28c8c1ab40391d572c09
unknown <mohamed.benighil07@gmail.com>
2021-01-12 19:57:37 +0100

    add Home & Notfound pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
-import React, { Fragment } from 'react';
-//import axios from 'axios'
+import React from 'react';
 import Navbar from './components/navbar/Navbar'
-import Users from './components/users/Users'
-import Search from './components/users/Search'
 import Alert from './components/Alert/Alert'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import About from './components/About/About'
 import User from './components/users/User'
 import GithubState from './context/github/GithubState'
 import AlertState from './context/alert/AlertState'
-
+import Home from './components/Home/Home'
+import Notfound from './components/Notfound/Notfound'
 
 const App = () => {
 
@@ -23,15 +21,10 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route exact path='/' render={ props => (
-                  <Fragment>
-                    <Search />
-                    <Users />
-                  </Fragment>
-                )}
-                />
+                <Route exact path='/' component={Home}/>
                 <Route exact path='/about' component={About}/>
                 <Route exact path='/user/:login'  component={User} />
+                <Route component={Notfound}/>
               </Switch>
             </div>
           </div>
